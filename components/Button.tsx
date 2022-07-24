7c9b9a1b5d85f6fda76b691232fdead224e93cd2
Jimmy Choi <jimmy@tossinvest.com>
2022-07-25 02:07:09 +0900

    feat: TailwindCSS & Emotion

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,3 +1,9 @@
-export default function Button() {
-  return <div>Button</div>
-}
+import styled from '@emotion/styled'
+
+const Button = styled.button`
+  padding: 16px;
+  border-radius: 8px;
+  background-color: hotpink;
+`
+
+export default Button
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Button from '@components/Button'
+import { css } from '@emotion/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
@@ -46,8 +48,24 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <input ref={inputRef} type="text" placeholder="name" />
-        <button onClick={handleClick}>Add Jacket</button>
+        <input
+          className="placeholder:italic placeholder:text-slate-400 block bg-white w-96 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+          ref={inputRef}
+          type="text"
+          placeholder="name"
+        />
+        <button
+          css={css`
+            background-color: hotpink;
+            padding: 16px;
+            border-radius: 8px;
+          `}
+          onClick={handleClick}
+        >
+          Add Jacket
+        </button>
+
+        <Button onClick={handleClick}>Add Jacket2</Button>
 
         <div>
           <p>Product List</p>
