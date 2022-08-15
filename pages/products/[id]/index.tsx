c8722a1e1c22d16c9bbc61e8acd1b6f6af8622e1
Jimmy Choi <jimmy@tossinvest.com>
2022-08-16 00:33:39 +0900

    fix: env

--- pages/products/[id]/index.tsx
+++ pages/products/[id]/index.tsx
@@ -24,13 +24,13 @@ import CommentItem from '@components/CommentItem'
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const product = await fetch(
-    `http://localhost:3000/api/get-product?id=${context.params?.id}`
+    `${process.env.NEXTAUTH_URL}/api/get-product?id=${context.params?.id}`
   )
     .then((res) => res.json())
     .then((data) => data.items)
 
   const comments = await fetch(
-    `http://localhost:3000/api/get-comments?productId=${context.params?.id}`
+    `${process.env.NEXTAUTH_URL}/api/get-comments?productId=${context.params?.id}`
   )
     .then((res) => res.json())
     .then((data) => data.items)
